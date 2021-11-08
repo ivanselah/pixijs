b07d716fb4a97b648b00a6b3f9916cbaefbfd6b1
songmin <>
2021-11-08 22:47:50 +0900

    Update Loader.js

--- 01/src/scripts/Loader.js
+++ 01/src/scripts/Loader.js
@@ -6,7 +6,9 @@ export class Loader {
   }
 
   preload() {
+    // 메서드를 호출, 로드에 필요한 모든 리소스를 지정함
     this.loader.add("bg", bg);
+    // 로드 메서드를 호출하여 첫 번째 단계에서 모든 리소스 로드
     this.loader.load((loader, resources) => {
       console.log(resources);
     });
