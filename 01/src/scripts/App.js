5fb1c8d56bc9d67985b7a52296e3c93cbd086bd1
songmin <>
2021-11-08 22:36:38 +0900

    pixj

---
+++ 01/src/scripts/App.js
@@ -0,0 +1,18 @@
+import * as PIXI from "pixi.js";
+import { Loader } from "./Loader.js";
+
+export class App {
+  run() {
+    /* create canvas */
+    // resizeTo: window 전체화면
+    this.app = new PIXI.Application({ resizeTo: window });
+    console.log(this.app);
+
+    // 캔버스를 html에 삽입
+    document.body.appendChild(this.app.view);
+
+    /* load sprites */
+    this.loader = new Loader(this.app.loader);
+    this.loader.preload();
+  }
+}
